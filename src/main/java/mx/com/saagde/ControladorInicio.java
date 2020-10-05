85ae5af3fad00aa545d207e499b6ef43542d7561
FabianGJ <fabiangj2@gmail.com>
2020-10-05 13:22:04 -0500

    Mi primer commit

---
+++ src/main/java/mx/com/saagde/ControladorInicio.java
@@ -0,0 +1,18 @@
+package mx.com.saagde;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class ControladorInicio {
+    
+    @GetMapping("/mostrar")
+    public String mostrar(){
+        log.info("Ejecutando el controlador rest");
+        log.debug("Entro a debug para mas detalle");
+    return "Hola mundo con Spring Boot";
+    }
+    
+}
